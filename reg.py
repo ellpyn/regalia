9c44a6aa8f491d2e9ea6ee14cb02869ea31c15e4
Ellen Hebert <ellh@protonmail.com>
2018-01-20 12:53:21 -0500

    Added recursion, used in * and + operators regularly

--- reg.py
+++ reg.py
@@ -1,39 +1,47 @@
 import sys
 
-pattern = 'he[lapdog]$'
-match = 'helloooooor'
+pattern = '^[baka]*oli$'
+match = 'oli'
 debug = True
 
 def tokenize(pattern):
     tokens = []
     alphabet = range(ord('a'), ord('z'))
 
-    for i in range(0, len(pattern)):
+    i = 0
+    while i < len(pattern):
         cc = pattern[i]
+        print i
 
         if ord(cc) in alphabet:
             tokens.append(['LETTER', cc])
         elif cc == '[':
+            global i
             i += 1
             patt = ''
             while not pattern[i] == ']':
                 patt += pattern[i]
                 i += 1
+                print i
             tokens.append(['BRACKET', patt])
+        elif cc == '(':
+            i += 1
+            patt = ''
+            while not pattern[i] == ']':
+                patt += pattern[i]
+                i += 1
+            tokens.append(['PAREN', [tokenize(patt)]])
         elif cc == '*':
-            prev = pattern[i - 1]
-            tokens.pop()
-            tokens.append(['PZERO', prev])
+            tokens.append(['PZERO', [tokens.pop()]])
         elif cc == '+':
-            prev = pattern[i - 1]
-            tokens.pop()
-            tokens.append(['PONE', prev])
+            tokens.append(['PONE', [tokens.pop()]])
         elif cc == '.':
             tokens.append(['WILDCARD'])
         elif cc == '$':
             tokens.append(['END'])
         elif cc == '^':
             tokens.append(['START'])
+        i+=1
     return tokens
 
 tokens = tokenize(pattern)
@@ -43,43 +51,65 @@ if debug:
     for token in tokens:
         print token
 
-index = 0
 if debug:
     print 'matching'
-for tsplit in tokens:
-    if tsplit[0] == 'LETTER':
-        letter = tsplit[1]
-        if letter == match[index]:
-            if debug:
-                print 'matched', letter
-            index += 1
-        else:
-            print 'FAILED to match', letter
-            sys.exit(0)
-    elif tsplit[0] == 'WILDCARD':
-        index += 1
-    elif tsplit[0] == 'PONE':
-        target = tsplit[1]
-        if match[index] == target:
-            while match[index] == target:
+
+
+def matches(match, tokens):
+    print 'comparing',tokens,'to',match
+    index = 0
+    for tsplit in tokens:
+        if tsplit[0] == 'LETTER':
+            letter = tsplit[1]
+            if letter == match[index]:
+                if debug:
+                    print 'matched', letter
                 index += 1
-                print 'pone matched', target
-        else:
-            print 'FAILED pone target not matched!'
-            sys.exit(0)
-    elif tsplit[0] == 'PZERO': # Kleene start
-        target = tsplit[1]
-        while match[index] == target:
+            else:
+                print 'FAILED to match', letter, 'to', match[index]
+                return False
+        elif tsplit[0] == 'WILDCARD':
             index += 1
-            if debug:
-                print 'pzero matched', target
-    elif tsplit[0] == 'START':
-        if not index == 0:
-            print 'FAILED not at start'
-            sys.exit(0)
-    elif tsplit[0] == 'END':
-        if not index == len(match):
-            print 'FAILED not at end'
-            sys.exit(0)
-print 'SUCCESS'
+        elif tsplit[0] == 'BRACKET':
+            print 'considering BRACKET'
+            target = tsplit[1]
+            print 'bracket',target,'matching',match[index]
+
+            if match[index] in target:
+                print 'bracket matched', target, 'to', match[index]
+                index += 1
+            else:
+                print 'FAILED to be in bracket'
+                return False
+        elif tsplit[0] == 'PONE':
+            target = tsplit[1]
+            if matches(match[index], target):
+                while index < len(match) and matches(match[index], target):
+                    index += 1
+                    if debug:
+                        print 'pone matched', target
+            else:
+                print 'FAILED pone target not matched!'
+                return False
+        elif tsplit[0] == 'PZERO': # Kleene star
+            target = tsplit[1]
+            print 'tokenpattern',target
+            print 'entering pzero'
+            while index < len(match) - 1 and matches(match[index], target) == True:
+                index += 1
+                if debug:
+                    print 'pzero matched', target
+        elif tsplit[0] == 'START':
+            if not index == 0:
+                print 'FAILED not at start'
+                return False
+        elif tsplit[0] == 'END':
+            if not index >= len(match):
+                print 'FAILED not at end'
+                return False
+
+    print 'SUCCESS'
+    return True
+
+matches(match, tokens)
 
